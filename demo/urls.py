a20b84611544565407f6928dffc4cba6b32c8852
raghavsinghalrs <22sep2001.rs@gmail.com>
2023-12-16 02:32:27 +0530

    CRUD OPERATIONS

--- demo/urls.py
+++ demo/urls.py
@@ -18,10 +18,21 @@ from django.contrib import admin
 from django.urls import path
 from home.views import *
 from veggie.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('',func,name="home"),
-    path('receipes/',reciepes,name='recipes'),
+    path('receipes/',receipes,name='recipes'),
+    path('delete-receipe/<id>/',delete_receipe,name="delete_receipe"),
+    path('update-receipe/<id>/',update_receipe,name="update_receipe"),
     path('contact/',func1,name="contact"),
     path('about/',func2,name="about"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    
+urlpatterns += staticfiles_urlpatterns()
--- veggie/views.py
+++ veggie/views.py
@@ -1,6 +1,50 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import *
+from django.http import HttpResponse
 # Create your views here.
 
-def reciepes(request):
-    return render(request,"recipes.html")
+def receipes(request):
+    if request.method == "POST":
+        data = request.POST
+        reciepe_images = request.FILES.get('reciepe_images')
+        reciepe_name = data.get('reciepe_name')
+        reciepe_desc = data.get('reciepe_desc')
+        print("name :",reciepe_name) 
+        print("description :",reciepe_desc)
+        print(reciepe_images)
+
+        reciepe.objects.create(
+            reciepe_name=reciepe_name,
+            reciepe_desc=reciepe_desc,
+            reciepe_images=reciepe_images,
+        )
+        return redirect("/receipes/")
+    
+    query = reciepe.objects.all()
+    if request.GET.get('search'):
+        query = query.filter(reciepe_name__icontains = request.GET.get('search'))
+    context = {'receipes': query}
+    return render(request,"recipes.html",context)
+
+def delete_receipe(request,id):
+    query = reciepe.objects.get(id=id)
+    query.delete()
+    return redirect("/receipes/")
+
+
+def update_receipe(request,id):
+    query = reciepe.objects.get(id=id)
+    if request.method == "POST":
+        data = request.POST
+        reciepe_images = request.FILES.get('reciepe_images')
+        reciepe_name = data.get('reciepe_name')
+        reciepe_desc = data.get('reciepe_desc')
+        query.reciepe_name = reciepe_name
+        query.reciepe_desc=reciepe_desc
+        if reciepe_images:
+            query.reciepe_images=reciepe_images
+        query.save()
+        return redirect("/receipes/")
+        
+    context = {'receipes':query}
+    return render(request,"update_reciepes.html",context)
